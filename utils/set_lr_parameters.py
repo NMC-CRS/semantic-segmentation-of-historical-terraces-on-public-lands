c959783ec004a948dc2734d01cfb9848b25f6f42
Claudine Gravel-Miguel <31354557+cgravelm@users.noreply.github.com>
2024-07-29 16:11:39 -0700

    Improve docstrings and set AdamW

--- utils/set_lr_parameters.py
+++ utils/set_lr_parameters.py
@@ -9,17 +9,24 @@ Created on Fri Mar 15 15:28:51 2024
 import torch.optim as optim
 
 def setup_parameter_learning_rate(model_structure, model):
-    """
-    Set up the Adam optimizer to the parameters that will learn
+    '''
+    Setup the fine tuning of pretrained weights
 
-        Parameters:
-            model_structure (str): The name of the model structure (should be UNet)
-            model (Pytorch model): The model itself
+    Parameters
+    ----------
+    model_structure : str
+        Type of model structure (e.g. UNet, MaskRCNN, FasterRCNN).
+    model : PyTorch model
+        Actual model structure.
 
-        Returns:
-            pretrained_optimizer (Pytorch optimizer): Pytorch optim optimizer of the frozen parameters
-            new_optimizer (Pytorch optimizer): Pytorch optim optimizer of the parameters that are learning
-    """
+    Returns
+    -------
+    pretrained_optimizer : Pytorch optimizer
+        Optimizer of the frozen parameters. Those do NOT get updated during training.
+    new_optimizer : Pytorch optimizer
+        Optimizer of the learning parameters, which gets updated during training.
+
+    '''
     
     if model_structure == "UNet":
         # Separate the parameters of the pretrained layers and the newly added layers
@@ -35,8 +42,8 @@ def setup_parameter_learning_rate(model_structure, model):
 
     # Define the optimizer
     # Create separate optimizer instances for each set of parameters
-    pretrained_optimizer = optim.Adam(pretrained_params, lr=0)  # Set learning rate to 0 to freeze
-    new_optimizer = optim.Adam(new_params, lr=1e-3)  # Use a desired learning rate for the new layers
+    pretrained_optimizer = optim.AdamW(pretrained_params, lr=0)  # Set learning rate to 0 to freeze
+    new_optimizer = optim.AdamW(new_params, lr=1e-3)  # Use a desired learning rate for the new layers
     
     return pretrained_optimizer, new_optimizer
 
